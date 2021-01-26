0feb8922300dcae3a26c1e89ebdc8d376b895458
merlinpratheesh <merlinpratheesh@gmail.com>
2021-01-26 17:34:07 +0530

    Online Working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, of, Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { UserdataService } from './service/userdata.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Goldenfile';
+  title = 'OnlineAuthOnetap';
+  myonline;
+  subjectonline = new BehaviorSubject(undefined);
+  getObservableonlineSub: Subscription = new Subscription;
+  getObservableonine = (localonline: Observable<boolean>) => {
+    this.getObservableonlineSub?.unsubscribe();
+    this.getObservableonlineSub = localonline.subscribe((valOnline: any) => {
+      console.log('19', valOnline);
+      this.subjectonline.next(valOnline);
+    });
+    return this.subjectonline;
+  }
+  OnlineCheck: undefined;
+    constructor(
+      public developmentservice: UserdataService)
+  {
+    this.myonline = this.getObservableonine(this.developmentservice.isOnline$);
+    this.OnlineCheck = this.myonline.pipe(
+      switchMap((onlineval: any) => {
+        console.log('31',onlineval);
+        if (onlineval === true) 
+        alert('Good Connection');
+        else{
+          alert('check internet Connection');
+
+        }
+        return of(onlineval);
+      }));
+
+  }
 }
