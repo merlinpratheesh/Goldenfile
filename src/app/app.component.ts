4dd3371a365cdf6673fd175c6821e7ad6117f487
merlinpratheesh <merlinpratheesh@gmail.com>
2021-01-27 11:37:39 +0530

    Public Projects Name description Worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,74 @@
-import { Component,OnInit } from '@angular/core';
-import {AngularFireAuth} from '@angular/fire/auth';
-
-import {FirebaseUISignInFailure, FirebaseUISignInSuccessWithAuthResult, FirebaseuiAngularLibraryService} from 'firebaseui-angular';
+import { AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { FormControl, Validators } from '@angular/forms';
+import firebase from 'firebase/app';
+import { BehaviorSubject, of, Subscription } from 'rxjs';
+import { map, switchMap, startWith, withLatestFrom } from 'rxjs/operators';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import {ChangeDetectionStrategy} from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
+import { projectControls,UserdataService } from './service/userdata.service';
 @Component({
-  selector: 'fbui-ng-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  constructor(private afAuth: AngularFireAuth, private firebaseuiAngularLibraryService: FirebaseuiAngularLibraryService) {
-  firebaseuiAngularLibraryService.firebaseUiInstance.disableAutoSignIn();
-}
+export class AppComponent implements OnInit,AfterViewInit,OnDestroy {
+  @ViewChild(MatAccordion) accordion: MatAccordion;
 
-    ngOnInit(): void {
-    //this.afAuth.authState.subscribe(d => console.log(d));
-  }
-  successCallback(data: FirebaseUISignInSuccessWithAuthResult) {
-    console.log('successCallback', data);
+  @Input() profileinfoUid: firebase.User;
+  myprojectControls: projectControls = {
+    publicprojectControl: new FormControl(null, Validators.required)
+  };
+ 
+  publicList = of(undefined);
+  localpublicList = [];
+  getPublicListSubscription: Subscription;
+  getPublicListBehaviourSub = new BehaviorSubject(undefined);
+  getPublicList = (publicProjects: AngularFirestoreDocument<any>) => {
+    if (this.getPublicListSubscription !== undefined) {
+      this.getPublicListSubscription.unsubscribe();
+    }
+    this.getPublicListSubscription = publicProjects.valueChanges().subscribe((val: any) => {
+      if (val === undefined) {
+        this.getPublicListBehaviourSub.next(undefined);
+      } else {
+        if (val.public.length === 0) {
+          this.getPublicListBehaviourSub.next(null);
+        } else {
+          this.localpublicList = val.public;
+          this.getPublicListBehaviourSub.next(val.public);
+        }
+      }
+    });
+    return this.getPublicListBehaviourSub;
+  };
 
-  }
+  constructor(    public developmentservice: UserdataService,private db: AngularFirestore) { }
+  publicProjsel:Subscription;
 
-  errorCallback(data: FirebaseUISignInFailure) {
-    console.warn('errorCallback', data);
+  ngOnInit(): void {
   }
+  ngAfterViewInit(){
+    this.publicProjsel = this.myprojectControls.publicprojectControl.valueChanges.pipe(
+      startWith(''),
+      map((publicProjectSelected: string) => {
+        if (!publicProjectSelected || publicProjectSelected === '') {
+          this.localpublicList = [];
+          this.getPublicListSubscription?.unsubscribe();
+          this.publicList = this.getPublicList(this.db.doc(('/projectList/publicProjects')));    
+          return publicProjectSelected;
+        }
+      })).subscribe(_=>{
 
-  uiShownCallback() {
-    console.log('UI shown');
+      });
   }
-  
-  logout() {
-    this.afAuth.signOut();
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.localpublicList, event.previousIndex, event.currentIndex);
+  }
+  ngOnDestroy(){
+    this.publicProjsel.unsubscribe();
+    this.getPublicListSubscription?.unsubscribe();
   }
 
 }
