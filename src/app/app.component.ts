ce79168d425e6961b2da43fa2d3e08277b1225c5
merlinpratheesh <merlinpratheesh@gmail.com>
2021-02-06 16:39:14 +0530

    worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { AfterViewInit, Component,OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, of, Subscription } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { UserdataService } from './service/userdata.service';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {FirebaseUISignInFailure, FirebaseUISignInSuccessWithAuthResult, FirebaseuiAngularLibraryService} from 'firebaseui-angular';
 @Component({
@@ -6,11 +9,45 @@ import {FirebaseUISignInFailure, FirebaseUISignInSuccessWithAuthResult, Firebase
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
-  constructor(public afAuth: AngularFireAuth, public firebaseuiAngularLibraryService: FirebaseuiAngularLibraryService) {
-    this.firebaseuiAngularLibraryService.firebaseUiInstance.disableAutoSignIn();
-}
 
+
+
+
+export class AppComponent implements AfterViewInit {
+  myonline;
+  subjectonline = new BehaviorSubject(undefined);
+  getObservableonlineSub: Subscription = new Subscription;
+
+  getObservableonline = (localonline: Observable<boolean>) => {
+    this.getObservableonlineSub?.unsubscribe();
+    this.getObservableonlineSub = localonline.subscribe((valOnline: any) => {
+      console.log(valOnline);
+      this.subjectonline.next(valOnline);
+    });
+    return this.subjectonline;
+  }
+  OnlineCheck: undefined;
+
+  constructor(public developmentservice: UserdataService, public afAuth: AngularFireAuth, public firebaseuiAngularLibraryService: FirebaseuiAngularLibraryService) {
+    this.firebaseuiAngularLibraryService.firebaseUiInstance.disableAutoSignIn();
+
+this.myonline = this.getObservableonline(this.developmentservice.isOnline$);
+this.OnlineCheck = this.myonline.pipe(
+  map((onlineval: any) => {
+    if(onlineval === false) 
+    {
+      //alert('check internet Connection');
+    }
+    else
+    {
+      //alert('good Connection');
+    }
+    
+
+    return (onlineval);
+  }));
+
+}
     ngAfterViewInit(): void {
       
   }
